a3d3bc1b4e8a8ba76aa947ff6e410fbb6ff06cf3
Whintali <whintali@gmail.com>
2022-05-24 08:23:35 +0200

    Some little changes

--- js/main.js
+++ js/main.js
@@ -1,60 +1,50 @@
-$(document).ready(function()
-{
-            // Ouvrir le modal
-    $('#open_modal').click(function() {
-        $('#modal_open').css(
-            {
-                'display' : 'block'
-            }
-        );
-    });
- 
-            // Fermer le modal
-    $('#close_modal').click(function() {
-        $('#modal_open').css(
-            {
-                'display' : 'none'
-            }
-        )
-    });
- 
- 
-    // Send Mail With Ajax
-    $('#send_email').click(function(e){
-        e.preventDefault();
-        var data = {
-            email: $('#email').val(),
-            name: $('#name').val(),
-            message: $('#message').val()
-        };
- 
-        //  AJAX
- 
-        $.ajax({
-            url: "mail.php",
-            type: 'POST',
-            data: data,
-            success: function(data) {
-                console.log('Success');
-                $("#js_alert_success").css({"display" : "block", "font-size" : "30px", 'font-weight' : 'bold', 'text-align' : 'center', 'color' : 'green', 'margin-top' : '50px'});
-                setTimeout(function(){
-                     
-                    $("#js_alert_success").css({"display" : "none", "font-size" : "30px", 'font-weight' : 'bold', 'text-align' : 'center', 'color' : 'green', 'margin-top' : '50px'});
-                    $('#email').val("");
-                    $('#name').val("");
-                    $('#message').val("")
-                }, 3000);
-            },
-            error: function(data) {
-                $("#js_alert_danger").css({"display" : "block", "font-size" : "30px", 'font-weight' : 'bold', 'text-align' : 'center', 'color' : 'red', 'margin-top' : '50px'});
-                setTimeout(function(){
-                    console.log('Error');
-                    $("#js_alert_danger").css({"display" : "none", "font-size" : "30px", 'font-weight' : 'bold', 'text-align' : 'center', 'color' : 'red', 'margin-top' : '50px'});
-                    $('#email').val("");
-                    $('#name').val("");
-                    $('#message').val("")
-                }, 3000);
-            }
-        });
-    });
-});
+// modal
+const openModal = document.querySelector('#open_modal');
+const openModal1 = document.querySelector('#open_modal1');
+const openModal2 = document.querySelector('#open_modal2');
+const openModal3 = document.querySelector('#open_modal3');
+const openModal4 = document.querySelector('#open_modal4');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.querySelector('#modal_open');
+const modal1 = document.querySelector('#modal_open1');
+const modal2 = document.querySelector('#modal_open2');
+const modal3 = document.querySelector('#modal_open3');
+const modal4 = document.querySelector('#modal_open4');
+const modalClose = document.querySelector('#modal_close');
+const modalClose1 = document.querySelector('#modal_close1');
+const modalClose2 = document.querySelector('#modal_close2');
+const modalClose3 = document.querySelector('#modal_close3');
+const modalClose4 = document.querySelector('#modal_close4');
+
+openModal.addEventListener('click', () => {
+    modal.classList.add('is-active');
+})
+openModal1.addEventListener('click', () => {
+    modal1.classList.add('is-active');
+})
+openModal2.addEventListener('click', () => {
+    modal2.classList.add('is-active');
+})
+openModal3.addEventListener('click', () => {
+    modal3.classList.add('is-active');
+})
+openModal4.addEventListener('click', () => {
+    modal4.classList.add('is-active');
+})
+
+
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+})
+modalClose1.addEventListener('click', () => {
+    modal1.classList.remove('is-active');
+})
+modalClose2.addEventListener('click', () => {
+    modal2.classList.remove('is-active');
+})
+modalClose3.addEventListener('click', () => {
+    modal3.classList.remove('is-active');
+})
+modalClose4.addEventListener('click', () => {
+    modal4.classList.remove('is-active');
+})
